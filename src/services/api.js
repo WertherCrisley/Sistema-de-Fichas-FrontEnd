669d31f1c4285400ef8e657117ea48e2eaecfe66
WertherCrisley <crisleyalencar7@gmail.com>
2026-07-11 18:42:11 -0300

    adicionado axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5003"
+    baseURL: "https://sistema-de-fichas-backend.onrender.com"
 });
 
 export default api;
